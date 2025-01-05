ad3266f156351e9021b3ebc6c704f1b161710f46
MGU-Anuradha <anuradh-se19049@stu.kln.ac.lk>
2025-01-05 13:15:11 +0530

    GN-30 - Make responsive and styles

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import {
   ThemeProvider,
   Grid,
@@ -8,20 +8,23 @@ import {
   InputBase,
   Button,
   FormControlLabel,
+  useMediaQuery,
+  Checkbox,
 } from "@mui/material";
 import theme from "../../styles/theme";
 import Header from "../../layout/Header";
 import authServiceImage from "../../assets/images/authService.jpg";
-import vendorRegister from "../../assets/images/why.png";
 import googleCalendar from "../../assets/images/google-calendar.png";
 import useStyles from "../../assets/css/style";
-import { CheckBox } from "@mui/icons-material";
 import SideNavBar from "../../layout/SideNavBar";
+import AppLogo from "../../components/shared/AppLogo";
 
 const RegisterForm = () => {
   const classes = useStyles();
+  const navigate = useNavigate();
   const location = useLocation();
   const { userType } = location.state || {};
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -63,7 +66,6 @@ const RegisterForm = () => {
       newErrors.mobileNumber = "Please enter a valid 10-digit mobile number.";
       formIsValid = false;
     }
-
     setErrors(newErrors);
     return formIsValid;
   };
@@ -80,19 +82,31 @@ const RegisterForm = () => {
   return (
     <ThemeProvider theme={theme}>
       <Grid container>
-        <SideNavBar />
-        <Grid item md={5}>
-          <Box
-            component="img"
-            src={userType === "vendor" ? vendorRegister : authServiceImage}
-            alt="Register"
-            sx={{
-              width: "100%",
-              height: "100%",
-              objectFit: "cover",
-            }}
-          />
-        </Grid>
+        {!isMobile && (
+          <Grid item md={5} sx={{ position: "relative" }}>
+            <Box
+              component="img"
+              src={authServiceImage}
+              alt="Register"
+              sx={{
+                width: "100%",
+                height: "100%",
+                objectFit: "cover",
+              }}
+            />
+            <Box
+              sx={{
+                position: "absolute",
+                top: "0px",
+                left: "20px",
+                zIndex: 1,
+              }}
+            >
+              <AppLogo size="lg" />
+            </Box>
+            <SideNavBar />
+          </Grid>
+        )}
 
         <Grid item md={7}>
           <Header />
@@ -102,9 +116,8 @@ const RegisterForm = () => {
             md={12}
             style={{ textAlign: "center", paddingTop: "40px" }}
           >
-            <Typography style={{ fontSize: "32px" }}>
-              Register as{" "}
-              {userType === "vendor" ? "a Vendor" : "an Event Planner"}
+            <Typography style={styles.sectionHeading}>
+              Register as {userType === "vendor" ? "a Vendor" : "an Event Planner"}
             </Typography>
           </Grid>
 
@@ -123,27 +136,31 @@ const RegisterForm = () => {
                   container
                   xs={12}
                   md={12}
+                  direction="row"
                   alignItems="center"
-                  spacing={2}
+                  spacing={1}
                   style={{ marginBottom: "10px" }}
                   key={index}
                 >
-                  <Grid item xs={12} md={4}>
+                  {/* Field Name */}
+                  <Grid item xs={4} md={4}>
                     <Typography
                       className={classes.typo}
-                      style={{ fontSize: "14px" }}
+                      style={{ fontSize: "14px", textAlign: "left", whiteSpace: "nowrap" }}
                     >
                       {field.charAt(0).toUpperCase() +
                         field.slice(1).replace(/([A-Z])/g, " $1")}
                       :
                     </Typography>
                   </Grid>
-                  <Grid item xs={12} md={8}>
+
+                  {/* Input Field */}
+                  <Grid item xs={8} md={8}>
                     <InputBase
                       className={classes.formInput}
+                      fullWidth
                       type={
-                        field.includes("password") ||
-                        field.includes("confirmPassword")
+                        field.includes("password") || field.includes("confirmPassword")
                           ? "password"
                           : "text"
                       }
@@ -162,120 +179,105 @@ const RegisterForm = () => {
               ))}
 
               {userType === "vendor" && (
-                <>
-                  <Grid
-                    container
-                    justifyContent="center"
-                    alignItems="center"
-                    style={{ marginTop: "20px" }}
-                  >
-                    <Grid item>
-                      <Typography
-                        style={{ fontSize: "13px", textAlign: "center" }}
-                      >
-                        Please authorize Genius to access your Google Calendar
-                        before proceeding. <br />
-                        More info..
-                      </Typography>
-                    </Grid>
-
-                    <Grid item style={{ marginTop: "20px" }}>
-                      <Button
-                        variant="contained"
-                        style={{
-                          backgroundColor: "#4C3A74",
-                          color: "#FFFFFF",
-                          padding: "10px 20px",
-                          fontSize: "14px",
-                          borderRadius: "8px",
-                          display: "flex",
-                          alignItems: "center",
-                          gap: "10px",
-                          textTransform: "none",
-                        }}
-                        onClick={() => console.log("Link Google Calendar")}
-                      >
-                        Link Google Calendar
-                        <img
-                          src={googleCalendar}
-                          alt="Google Calendar Icon"
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                          }}
-                        />
-                      </Button>
-                    </Grid>
+                <Grid
+                  container
+                  justifyContent="center"
+                  alignItems="center"
+                  style={{ marginTop: "20px" }}
+                >
+                  <Grid item>
+                    <Typography
+                      style={{
+                        fontSize: "14px",
+                        textAlign: "center",
+                      }}
+                    >
+                      Please authorize Genius to access your Google Calendar
+                      before proceeding. More info..
+                    </Typography>
                   </Grid>
-                </>
+
+                  <Grid item style={{ marginTop: "20px" }}>
+                    <Button
+                      variant="contained"
+                      style={styles.googleButton}
+                      onClick={() => console.log("Link Google Calendar")}
+                    >
+                      Link Google Calendar
+                      <img
+                        src={googleCalendar}
+                        alt="Google Calendar Icon"
+                        style={{
+                          width: "20px",
+                          height: "20px",
+                        }}
+                      />
+                    </Button>
+                  </Grid>
+                </Grid>
               )}
 
               {/* Terms and Conditions Checkbox */}
-              <Grid
-                item
+              <Grid item
                 container
                 xs={12}
                 justifyContent="center"
+                spacing={2}
                 style={{ marginTop: "20px", marginBottom: "10px" }}
               >
-                <FormControlLabel
-                  control={
-                    <CheckBox
-                      checked={formData.acceptTerms}
-                      onChange={handleChange}
-                      name="acceptTerms"
-                      color="primary"
-                    />
-                  }
-                  label={
-                    <span style={{ fontSize: "14px" }}>
-                      Do you agree to our{" "}
-                      <span
-                        style={{ color: "blue", cursor: "pointer" }}
-                        onClick={() =>
-                          console.log("Navigate to Privacy and Policies")
-                        }
-                      >
-                        Privacy and Policies
+                  <FormControlLabel
+                    control={
+                      <Checkbox
+                        checked={formData.acceptTerms}
+                        onChange={handleChange}
+                        name="acceptTerms"
+                        sx={{ color: "#fff" }}
+                      />
+                    }
+                    label={
+                      <span style={{ fontSize: "14px" }}>
+                        Do you agree to our{" "}
+                        <span
+                          style={{ color: "#007ADF", cursor: "pointer" }}
+                          onClick={() =>
+                            console.log("Navigate to Privacy and Policies")
+                          }
+                        >
+                          Privacy and Policies
+                        </span>
+                        ?
                       </span>
-                      ?
-                    </span>
-                  }
-                />
+                    }
+                    sx={{ color: "#fff" }}
+                  />
               </Grid>
 
               {/* Submit Button */}
               <Grid item container xs={12} justifyContent="center">
-                <Button
-                  type="submit"
-                  className={classes.submitButton}
-                  // onClick={handleRegisterClick}
-                >
+                <Button type="submit" style={styles.button}>
                   Register
                 </Button>
               </Grid>
             </Grid>
           </form>
 
-          {/* Login Section (Button and Text on the Same Line) */}
+          {/* Login Section */}
           <Grid
             container
             spacing={1}
             alignItems="center"
             justifyContent="center"
-            style={{ marginTop: "30px" }}
+            style={{ marginTop: "40px", marginBottom: "40px" }}
           >
             <Grid item>
-              <Typography className={classes.typo} style={{ fontSize: "14px" }}>
+              <Typography style={{ fontSize: "14px" }}>
                 Do you have an account?
               </Typography>
             </Grid>
             <Grid item>
               <Button
-                type="submit"
-                className={classes.submitButton}
-                style={{ backgroundColor: "#B07207" }}
-                // onClick={handleRegisterClick}
+                onClick={() => navigate("/login")}
+                style={styles.linkButton}
               >
                 Login
               </Button>
@@ -288,3 +290,45 @@ const RegisterForm = () => {
 };
 
 export default RegisterForm;
+
+
+export const styles = {
+  button: {
+    backgroundColor: "#4C3A74",
+    color: "#FFFFFF",
+    padding: "10px 30px",
+    fontSize: "16px",
+    borderRadius: "8px",
+    width: "220px",
+    textTransform: "none",
+    border: "2px solid #a0a5ee",
+    marginTop: "20px"
+  },
+  linkButton: {
+    backgroundColor: "#B07207",
+    color: "#FFFFFF",
+    padding: "8px 20px",
+    fontSize: "14px",
+    borderRadius: "8px",
+    textTransform: "none",
+  },
+  googleButton: {
+    backgroundColor: "#4C3A74",
+    color: "#FFFFFF",
+    padding: "10px 20px",
+    fontSize: "14px",
+    borderRadius: "8px",
+    display: "flex",
+    alignItems: "center",
+    gap: "10px",
+    textTransform: "none",
+  },
+  errorText: {
+    color: "red",
+    fontSize: "12px",
+  },
+  sectionHeading: {
+    fontSize: "42px",
+    marginBottom: "25px",
+  },
+};
