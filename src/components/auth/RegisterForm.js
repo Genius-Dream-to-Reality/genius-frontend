f6f3c277f1e3069c4257d112a878d3e0a11a140d
MMPjayarathne <pramudithajayarathna@gmail.com>
2025-04-14 22:14:35 +0530

    Update the register by integrating the apis and oauth with google calender

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { useLocation } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { Navigate, useLocation } from "react-router-dom";
 import {
   ThemeProvider,
   Grid,
@@ -15,13 +15,26 @@ import authServiceImage from "../../assets/images/authService.jpg";
 import vendorRegister from "../../assets/images/why.png";
 import googleCalendar from "../../assets/images/google-calendar.png";
 import useStyles from "../../assets/css/style";
-import { CheckBox } from "@mui/icons-material";
+import { Checkbox } from "@mui/material";
 import SideNavBar from "../../layout/SideNavBar";
+import { customerRegistration, vendorRegistration } from "../../utils/auth";
+import { useNavigate } from "react-router-dom";
 
 const RegisterForm = () => {
+  const redirect_uri = process.env.REACT_APP_GOOGLE_CALENDER_REDIRECT_URI;
+  const client_id = process.env.REACT_APP_GOOGLE_CALENDER_CLIENT_ID;
   const classes = useStyles();
   const location = useLocation();
-  const { userType } = location.state || {};
+  const { userType, from, oauthData } = location.state || {};
+  const [message, setMessage] = useState("");
+  const [isError, setIsError] = useState(false);
+  const [isGoogleCalendarLinked, setIsGoogleCalendarLinked] = useState(false);
+  const navigate = useNavigate();
+
+  const [calendarId, setCalendarId] = useState(null);
+  const [accessToken, setAccessToken] = useState(null);
+  const [refreshToken, setRefreshToken] = useState(null);
+  const [tokenExpiry, setTokenExpiry] = useState(null);
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -38,12 +51,46 @@ const RegisterForm = () => {
     mobileNumber: "",
   });
 
-  const handleChange = (e) => {
-    const { name, value, type, checked } = e.target;
-    setFormData({
-      ...formData,
+  useEffect(() => {
+    const savedForm = localStorage.getItem("registerFormData");
+
+    if (savedForm) {
+      setFormData(JSON.parse(savedForm));
+    }
+    if (from === "callback") {
+      //console.log("Auth Code from callbackpage: ", oauthData);
+
+      if (oauthData) {
+        try {
+          setCalendarId(oauthData.calendarId);
+          setAccessToken(oauthData.accessToken);
+          setRefreshToken(oauthData.refreshToken);
+          setTokenExpiry(oauthData.tokenExpiry);
+          //console.log("AccessToken: ", accessToken);
+          setIsGoogleCalendarLinked(true);
+        } catch (err) {
+          console.error("Failed to parse authObject:", err);
+        }
+      } else {
+        console.log("No auth code found in local storage.");
+      }
+    }
+  }, [from]);
+
+ 
+
+  const handleChange = (event) => {
+    const { name, value, checked, type } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
       [name]: type === "checkbox" ? checked : value,
-    });
+    }));
+  };
+
+  const handleGoogleCalendar = () => {
+    localStorage.setItem("registerFormData", JSON.stringify(formData));
+    const googleAuthUrl = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=code&scope=https://www.googleapis.com/auth/calendar&access_type=offline&prompt=consent`;
+    window.location.href = googleAuthUrl;
   };
 
   const validateForm = () => {
@@ -68,11 +115,64 @@ const RegisterForm = () => {
     return formIsValid;
   };
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-
+    if (!formData.acceptTerms) {
+      setMessage("You must accept the terms and conditions to continue.");
+      setIsError(true);
+      return; // stop submission
+    }
     if (validateForm()) {
       const { confirmPassword, ...dataToSubmit } = formData;
+      if (userType === "vendor") {
+        const data = {
+          name: formData.firstName + " " + formData.lastName,
+          email: formData.email,
+          phoneNumber: formData.mobileNumber,
+          password: formData.password,
+          acceptTerms: formData.acceptTerms,
+          calendarId: calendarId,
+          accessToken: accessToken,
+          refreshToken: refreshToken,
+          tokenExpiry: tokenExpiry,
+        };
+        const res = await vendorRegistration(data);
+        if (res.type === "error") {
+          console.log("An error occurred:", res.message);
+          setMessage(res.message);
+          setIsError(true);
+        } else {
+          console.log("Success:", res.message);
+          setMessage("Registration successful!");
+          setIsError(false);
+          navigate("/otp", {
+            state: { userType: "vendor", email: formData.email },
+          });
+        }
+      } else {
+        const data = {
+          name: formData.firstName + " " + formData.lastName,
+          email: formData.email,
+          mobileNo: formData.mobileNumber,
+          password: formData.password,
+          acceptTerms: formData.acceptTerms,
+        };
+
+        const res = await customerRegistration(data);
+        if (res.type === "error") {
+          console.log("An error occurred:", res.message);
+          setMessage(res.message);
+          setIsError(true);
+        } else {
+          console.log("Success:", res.message);
+          setMessage("Registration successful!");
+          setIsError(false);
+          localStorage.removeItem("registerFormData");
+          navigate("/otp", {
+            state: { userType: "customer", email: formData.email },
+          });
+        }
+      }
       console.log("Form Data:", dataToSubmit);
     }
   };
@@ -108,7 +208,7 @@ const RegisterForm = () => {
             </Typography>
           </Grid>
 
-          <form onSubmit={handleSubmit}>
+          <form>
             <Grid container className={classes.formContent}>
               {[
                 "firstName",
@@ -162,23 +262,31 @@ const RegisterForm = () => {
               ))}
 
               {userType === "vendor" && (
-                <>
-                  <Grid
-                    container
-                    justifyContent="center"
-                    alignItems="center"
-                    style={{ marginTop: "20px" }}
-                  >
-                    <Grid item>
-                      <Typography
-                        style={{ fontSize: "13px", textAlign: "center" }}
-                      >
-                        Please authorize Genius to access your Google Calendar
-                        before proceeding. <br />
-                        More info..
-                      </Typography>
-                    </Grid>
+                <Grid
+                  container
+                  justifyContent="center"
+                  alignItems="center"
+                  style={{ marginTop: "20px" }}
+                >
+                  <Grid item>
+                    <Typography
+                      style={{ fontSize: "13px", textAlign: "center" }}
+                    >
+                      {isGoogleCalendarLinked ? (
+                        <>
+                          Your Google Calendar is linked successfully. <br />
+                        </>
+                      ) : (
+                        <>
+                          Please authorize Genius to access your Google Calendar
+                          before proceeding. <br />
+                          More info..
+                        </>
+                      )}
+                    </Typography>
+                  </Grid>
 
+                  {!isGoogleCalendarLinked && (
                     <Grid item style={{ marginTop: "20px" }}>
                       <Button
                         variant="contained"
@@ -193,21 +301,18 @@ const RegisterForm = () => {
                           gap: "10px",
                           textTransform: "none",
                         }}
-                        onClick={() => console.log("Link Google Calendar")}
+                        onClick={handleGoogleCalendar}
                       >
                         Link Google Calendar
                         <img
                           src={googleCalendar}
                           alt="Google Calendar Icon"
-                          style={{
-                            width: "20px",
-                            height: "20px",
-                          }}
+                          style={{ width: "20px", height: "20px" }}
                         />
                       </Button>
                     </Grid>
-                  </Grid>
-                </>
+                  )}
+                </Grid>
               )}
 
               {/* Terms and Conditions Checkbox */}
@@ -220,10 +325,10 @@ const RegisterForm = () => {
               >
                 <FormControlLabel
                   control={
-                    <CheckBox
+                    <Checkbox
+                      name="acceptTerms"
                       checked={formData.acceptTerms}
                       onChange={handleChange}
-                      name="acceptTerms"
                       color="primary"
                     />
                   }
@@ -249,13 +354,30 @@ const RegisterForm = () => {
                 <Button
                   type="submit"
                   className={classes.submitButton}
-                  // onClick={handleRegisterClick}
+                  onClick={handleSubmit}
                 >
                   Register
                 </Button>
               </Grid>
             </Grid>
           </form>
+
+          {message && (
+            <Grid
+              item
+              xs={12}
+              style={{ textAlign: "center", marginTop: "20px" }}
+            >
+              <Typography
+                style={{
+                  color: isError ? "red" : "green",
+                  fontSize: "14px",
+                }}
+              >
+                {message}
+              </Typography>
+            </Grid>
+          )}
 
           {/* Login Section (Button and Text on the Same Line) */}
           <Grid
